ac31c2594f3ada87e9892bf6f8719ad21f54180c
everton.batista <evertonbatistam@gmail.com>
2025-04-18 03:48:15 -0300

    Implementação inicial do kafka Adicionado dependencias e configurações

---
+++ src/main/java/br/com/batista/desafio01/service/kafka/KafkaConsumer.java
@@ -0,0 +1,13 @@
+package br.com.batista.desafio01.service.kafka;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "notify", groupId = "desafio01")
+    public void listen(String message) {
+        System.out.println("Received message: " + message);
+    }
+}
