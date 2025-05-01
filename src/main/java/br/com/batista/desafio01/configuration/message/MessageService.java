e2fce5a2e918ddf7c8e36ce60923ff12401329f0
everton.batista <evertonbatistam@gmail.com>
2025-05-01 09:15:24 -0300

    Refatorado classes de tratativas de error GlobalExceptionHandler junto de MessageService - Incluido chaves no messages - Ajustado constantes

--- src/main/java/br/com/batista/desafio01/configuration/message/MessageService.java
+++ src/main/java/br/com/batista/desafio01/configuration/message/MessageService.java
@@ -1,7 +1,6 @@
 package br.com.batista.desafio01.configuration.message;
 
 import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.Locale;
@@ -9,7 +8,7 @@ import java.util.Locale;
 @Service
 public class MessageService {
 
-    Locale defaultLocale = Locale.US;
+    private static final Locale defaultLocale = Locale.US;
 
     private final MessageSource messageSource;
 
@@ -19,6 +18,10 @@ public class MessageService {
 
     public String getMessage(String key) {
         return messageSource.getMessage(key, null, defaultLocale);
+    }
+
+    public String getMessage(String key, String[] args) {
+        return messageSource.getMessage(key, args, defaultLocale);
     }
 
     public String getMessage(String key, Locale locale) {
