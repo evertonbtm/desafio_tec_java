ddc30dc5dd8127c9da1df12c1769dceca4034e1e
everton.batista <evertonbatistam@gmail.com>
2025-05-01 08:24:20 -0300

    Refatorando AuthController

--- src/main/java/br/com/batista/desafio01/controller/AuthController.java
+++ src/main/java/br/com/batista/desafio01/controller/AuthController.java
@@ -3,7 +3,6 @@ package br.com.batista.desafio01.controller;
 import br.com.batista.desafio01.configuration.auth.JwtAuthUtil;
 import br.com.batista.desafio01.model.dto.auth.AuthDTO;
 import br.com.batista.desafio01.model.dto.auth.AuthResponseDTO;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -11,23 +10,22 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
+    private final AuthenticationManager authenticationManager;
 
-    @Autowired
-    private JwtAuthUtil jwtUtil;
+    private final UserDetailsService userDetailsService;
 
-    @Autowired
-    private UserDetailsService userDetailsService;
+    public AuthController( UserDetailsService userDetailsService, AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+        this.userDetailsService = userDetailsService;
+    }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody AuthDTO request) {
+    public ResponseEntity<AuthResponseDTO> login(@RequestBody AuthDTO request) {
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword())
         );
